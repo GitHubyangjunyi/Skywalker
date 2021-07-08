80c8ddeaf5ea9081674f119f91d27f1956de56f5
杨俊艺 <tim_yang@intretech.com>
2021-07-08 19:01:03 +0800

    Initial Commit

---
+++ Sources/Skywalker/Skywalker.swift
@@ -0,0 +1,3 @@
+struct Skywalker {
+    var text = "Hello, World!"
+}
---
+++ Tests/SkywalkerTests/SkywalkerTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import Skywalker
+
+    final class SkywalkerTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+            XCTAssertEqual(Skywalker().text, "Hello, World!")
+        }
+    }
